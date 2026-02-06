cf97f172fe4d83c5b558887ae43ce4b6d21d683f
Carmen <carmen.nuvoli@mat.tn.it>
2026-02-06 09:34:44 +0100

    Aggiunta funzionalità POST

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
+interface PostData {
+      email: string;
+      password: string;
+  }
+
 function App() {
+  const [data, setData] = useState<PostData>({ email: "carmen@asistar.it", password: "admin12345" });
+  const [response, setResponse] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
+  
+  const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault();
+      setLoading(true);
+  
+      try {
+          const res = await fetch("https://shiftcaller.it/api/mockup-login", {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json",
+              },
+              body: JSON.stringify(data),
+          });
+      const result = await res.json();
+      setResponse(result);
+      } catch (error) {
+          setResponse({ error: "Errore" });
+      } finally {
+          setLoading(false);
+      }
+  };
+    
+/*
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
@@ -17,9 +48,11 @@ function App() {
     console.log("Email: ", email);
     console.log("Password: ", password);
   }
+  */
 
   return (
     <>
+    <form onSubmit={handleSubmit}>
       <div className='Login'>
         <div className='LoginBox'>
           <div className='LoginHeader'>
@@ -28,13 +61,15 @@ function App() {
           <div className='LoginBody'>
             <div className='inputs'>
               <input 
-                onChange={(e: any) => handleEmail(e)} 
+                value={data.email}
+                onChange={(e: any) => setData({...data, email: e.target.value})} 
                 className="email" 
                 type="text" 
                 placeholder='Inserisci nome utente o email'
               />
               <input 
-                onChange={(e: any) => handlePassword(e)} 
+                value={data.password}
+                onChange={(e: any) => setData({...data, password: e.target.value})} 
                 className="password" 
                 type="password" 
                 placeholder='Inserisci la password'
@@ -43,16 +78,20 @@ function App() {
             <br />
             <div>
               <button 
-                onClick={handleClick}
-                className='submitButton'>
-                  Login
+                type="submit"
+                className='submitButton'
+                disabled={loading}>
+                  {loading ? "Invio..." : "Login"}
               </button>
             </div>
           </div>
         </div>
       </div>
+    </form>
     </>
   )
 }
+
+//{response && <pre>{JSON.stringify(response, null, 2)}</pre>}
 
 export default App
